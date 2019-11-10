70e7c14d33ee52ad9c5834eab23570a75b47ea98
Max Goodman <max.goodman@flatironschool.com>
2019-11-10 11:22:53 -0500

    adding rinkeby network to config file

--- truffle.js
+++ truffle.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HDWalletProvider = require("truffle-hdwallet-provider");
 
 const infuraURL = "https://mainnet.infura.io/v3/dbcb5a76dfc447e79201a60923e4a64c";
+const infuraRinkebyURL = "https://rinkeby.infura.io/v3/dbcb5a76dfc447e79201a60923e4a64c";
 const mnemonic = process.env.PRIV_SEED;
 
 module.exports = {
@@ -14,6 +15,10 @@ module.exports = {
       port: 7545,
       network_id: "*"
     },
+    "rinkeby":{
+      provider: ()=> new HDWalletProvider(mnemonic, infuraRinkebyURL),
+      network_id: 4
+    }
     "mainnet":{
       provider: () => new HDWalletProvider(mnemonic, infuraURL),
       network_id: 1
